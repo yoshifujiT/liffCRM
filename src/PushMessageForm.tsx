5730e97d1fb5a1c974638530022eaabb5f1b47c2
yoshifuji <yoshifujitooru@wwsyoshifuji.local>
2019-09-19 22:11:44 +0900

    uidとselectboxを結びつけ

--- src/PushMessageForm.tsx
+++ src/PushMessageForm.tsx
@@ -2,74 +2,76 @@ import React, { useState, useEffect } from 'react'
 import { animals } from './config/animal-config'
 import MultiSelect from './MultiSelect'
 
-type Votes = {
-  all: Array<object>;
-  dog: Array<object>;
-  cat: Array<object>;
-}
+type To = Array<{
+  value: string;
+  label: string;
+}>
 type Props = {
-  votes: Votes
+  votes: {
+    all: Array<object>;
+    dog: Array<object>;
+    cat: Array<object>;
+  }
 }
-type User_ids = {
+type UserIds = {
   dog: Array<string>;
   cat: Array<string>;
 }
-type Animal = keyof User_ids
+type Animal = keyof UserIds
 
-const PushMessageForm: React.FC<Props> = (props: Props) => {
+const PushMessageForm: React.FC<Props> = (props) => {
   const { votes } = props
 
-  const [to, setTo] = useState(new Set())
-  const [selected, setSelected] = useState(new Set<Animal>())
-  const [user_ids, setUserIds] = useState<User_ids>({
+  const [to, setTo] = useState<To>([])
+  const [selected, setSelected] = useState<Array<Animal>>([])
+  const [userIds, setUserIds] = useState<UserIds>({
     dog: [],
     cat: [],
   })
 
+  // 宛先のUserID配列を生成: to
   useEffect(() => {
-    const user_set = to
-    const uids = user_ids
-    user_set.clear()
-
-    selected.forEach((animal) => {
-      const ids = uids[animal]
-      ids.forEach((id) => {
-        user_set.add(id)
-      })
+    const ids = selected.map((animal) => {
+      return userIds[animal]
     })
-    setTo(user_set)
-    
-  }, [to, selected, user_ids])
+    // uniqueなuid配列
+    const basearr = [
+      ...new Set(ids.flat())
+    ]
+    const newTo = basearr.map((a) => {
+      return { value: a, label: a }
+    })
+    setTo(newTo)
+  }, [selected, userIds])
 
+  // animalごとのUserID配列を準備: userIds
   useEffect(() => {
     // TODO: anyを使わない実装
-    // firestoreから取得した個別ドキュメントの型を入れる
-    const obj = user_ids
+    let tmp = userIds
     animals.forEach((key) => {
       const animal: Animal = key as Animal
-      votes[animal].forEach((vote: any) => {
-        obj[animal].push(vote.id)
+      // TODO: firestoreから取得した個別ドキュメントの型を入れる
+      let newArr = votes[animal].map((vote: any) => {
+        return vote.id
       })
+      tmp[animal] = newArr
     })
-    setUserIds(obj)
-  }, [user_ids, votes])
+    setUserIds(tmp)
+  }, [userIds, votes])
 
-
-  const toggleSelected = (e: React.ChangeEvent, animal: Animal) => {
-    const newSet: Set<Animal> = new Set()
-    selected.forEach((animal) => {
-      newSet.add(animal)
-    })
-
+  const toggleSelected = (e: React.ChangeEvent) => {
     const target = e.target as HTMLInputElement
+    const animal = target.name as Animal
     const value: boolean = target.checked
+    let newSet = new Set(selected)
+
     if (value) {
       newSet.add(animal)
     } else {
       newSet.delete(animal)
     }
-    
-    setSelected(newSet)
+
+    setSelected([...newSet])
   }
 
   return (
@@ -77,12 +79,12 @@ const PushMessageForm: React.FC<Props> = (props: Props) => {
       <p>Pushメッセージを送信</p>
       <div id="animal_keys">
         {/* <label><input name="all" type="checkbox" onChange={addSelectedAll} />すべて</label> */}
-        <label><input name="dog" type="checkbox" onChange={(e) => toggleSelected(e, 'dog')} />犬派</label>
-        <label><input name="cat" type="checkbox" onChange={(e) => toggleSelected(e, 'cat')} />猫派</label>
+        <label><input name="dog" type="checkbox" onChange={toggleSelected} />犬派</label>
+        <label><input name="cat" type="checkbox" onChange={toggleSelected} />猫派</label>
       </div>
       <form action="/push" method="POST">
-        <MultiSelect uids={[{value: 'hoge', label: 'yoyoyo'}]} />
-        <textarea style={{width: '500px', height: '100px', display: 'block', margin: '0 auto 20px'}} name="message" id="message"></textarea>
+        <MultiSelect uids={to} />
+        <textarea style={{ width: '500px', height: '100px', display: 'block', margin: '0 auto 20px' }} name="message" id="message"></textarea>
         <button>送信する</button>
       </form>
     </div>
