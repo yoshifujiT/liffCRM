a9fb461c519dd811d799d64056c62c6905fcd1a9
yoshifuji <yoshifujitooru@wwsyoshifuji.local>
2019-09-22 17:58:46 +0900

    express導入

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,10 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions'
+import express from 'express'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express()
+
+app.get('/test', (req: any, res:any) => {
+  res.send('test')
+})
+
+exports.app = functions.https.onRequest(app)
